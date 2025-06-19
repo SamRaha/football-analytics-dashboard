580bcf499973c34ea83503ec41900ca4fd76f228
Sam Raha <sam.raha@hotmail.co.uk>
2025-06-19 16:17:03 +0100

    restyle format

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.scss";
-import "./variables.scss";
+import "./styles/variables.scss";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
