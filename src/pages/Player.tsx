d2748e5cccc1f806dffc899a3af6d6d060fcd806
Sam Raha <sam.raha@hotmail.co.uk>
2025-06-19 12:03:59 +0100

    styling

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -21,46 +21,52 @@ const Player: React.FC = () => {
 
     return (
         <div className="player-page">
-            <div className="content">
-                <Link to="/" className="back-link">
-                    ← Back to search
-                </Link>
-                <div className="player-info">
-                    <div className="col-image">
-                        <img className="player-image" src={player.img} alt={`${player.name} profile`} onError={(e) => (e.currentTarget.src = "/images/placeholder.png")} />
-                        <h5 className="player-name text-nowrap">{player.name}</h5>
-                    </div>
+            <Link to="/" className="header">
+                <div className="back-link">← Back to search</div>
+            </Link>
+            <div>
+                <div className="player-info-background">
+                    <div className="player-info">
+                        <div className="col-image">
+                            <img className="player-image" src={player.img} alt={`${player.name} profile`} onError={(e) => (e.currentTarget.src = "/images/placeholder.png")} />
+                            <h5 className="player-name text-nowrap">{player.name}</h5>
+                        </div>
 
-                    <div className="col-stats">
-                        <p>
-                            <strong>Age:</strong> {player.age}
-                        </p>
-                        <p>
-                            <strong>Goals:</strong> {player.totalgoals}
-                        </p>
-                        <p>
-                            <strong>Caps:</strong> {player.totalcaps}
-                        </p>
-                    </div>
+                        <div className="col-stats">
+                            <p>
+                                <strong>Age:</strong> {player.age}
+                            </p>
+                            <p>
+                                <strong>Goals:</strong> {player.totalgoals}
+                            </p>
+                            <p>
+                                <strong>Caps:</strong> {player.totalcaps}
+                            </p>
+                        </div>
 
-                    <div className="col-contract">
-                        <p>
-                            <strong>Contracted to</strong> {player.club}
-                        </p>
-                        <p>
-                            <strong>Salary:</strong> £{player.salary.toLocaleString()} Annually
-                        </p>
-                    </div>
-                    <div className="col-scout">
-                        <div className="player-summary">
-                            <strong>Scout Summary</strong>
+                        <div className="col-contract">
+                            <p>
+                                <strong>Contracted to</strong> {player.club}
+                            </p>
+                            <p>
+                                <strong>Salary:</strong> £{player.salary.toLocaleString()} Annually
+                            </p>
+                        </div>
+                        <div className="col-scout">
+                            <div className="player-summary">
+                                <strong>Scout Summary</strong>
 
-                            <div className="mb-1" />
-                            <p>{player.scout_summary}</p>
+                                <div className="mb-1" />
+                                <p>{player.scout_summary}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <StatsTables metrics={player.metrics} />
+            </div>
+            <div className="stats-container">
+                <div className="stats">
+                    <StatsTables metrics={player.metrics} />
+                </div>
             </div>
         </div>
     );
